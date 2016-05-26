9da5f2c0dfd5790d6615f40571c55c7eb725141d
codercecilia <cecilia@coder-kids.com>
2016-05-26 14:16:47 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,23 @@ var braintree = require('braintree');
 var router = express.Router(); // eslint-disable-line new-cap
 var gateway = require('../lib/gateway');
 
-var TRANSACTION_SUCCESS_STATUSES = [
-  braintree.Transaction.Status.Authorizing,
-  braintree.Transaction.Status.Authorized,
-  braintree.Transaction.Status.Settled,
-  braintree.Transaction.Status.Settling,
-  braintree.Transaction.Status.SettlementConfirmed,
-  braintree.Transaction.Status.SettlementPending,
-  braintree.Transaction.Status.SubmittedForSettlement
-];
+// var TRANSACTION_SUCCESS_STATUSES = [
+//   braintree.Transaction.Status.Authorizing,
+//   braintree.Transaction.Status.Authorized,
+//   braintree.Transaction.Status.Settled,
+//   braintree.Transaction.Status.Settling,
+//   braintree.Transaction.Status.SettlementConfirmed,
+//   braintree.Transaction.Status.SettlementPending,
+//   braintree.Transaction.Status.SubmittedForSettlement
+// ];
+
+  var SUBSCRIPTION_SUCCESS_STATUSES = []
+    braintree.Subscription.Status.Active
+    braintree.Subscription.Status.Canceled,
+    braintree.Subscription.Status.Expired,
+    braintree.Subscription.Status.PastDue,
+    braintree.Subscription.Status.Pending
+   ];
 
 function formatErrors(errors) {
   var formattedErrors = '';
@@ -27,11 +35,15 @@ function formatErrors(errors) {
   return formattedErrors;
 }
 
-function createResultObject(transaction) {
+// function createResultObject(transaction) {
+function createResultObject(subscription) {
   var result;
-  var status = transaction.status;
+  // var status = transaction.status;
 
-  if (TRANSACTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
+  // if (TRANSACTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
+  var status = subscription.status;
+
+  if (SUBSCRIPTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
     result = {
       header: 'Sweet Success!',
       icon: 'success',
@@ -60,11 +72,15 @@ router.get('/checkouts/new', function (req, res) {
 
 router.get('/checkouts/:id', function (req, res) {
   var result;
-  var transactionId = req.params.id;
-
-  gateway.transaction.find(transactionId, function (err, transaction) {
-    result = createResultObject(transaction);
-    res.render('checkouts/show', {transaction: transaction, result: result});
+  // var transactionId = req.params.id;
+  // gateway.transaction.find(transactionId, function (err, transaction) {
+  //   result = createResultObject(transaction);
+  //   res.render('checkouts/show', {transaction: transaction, result: result});
+  // });
+  var subscriptionId = req.params.id;
+  gateway.subscription.find(subscriptionId, function (err, subscription) {
+    result = createResultObject(subscription);
+    res.render('checkouts/show', {subscription: subscription, result: result});
   });
 });
 
@@ -72,48 +88,47 @@ router.post('/checkouts', function (req, res) {
   var transactionErrors;
   var amount = req.body.amount; // In production you should not take amounts directly from clients
   var nonce = req.body.payment_method_nonce;
-  var cardholderName = req.body.cardholderName;
   var startDate = new Date(Date.UTC(2016, 8, 6, 0, 0, 0));
 
-  // gateway.customer.create({
-  // paymentMethodNonce: nonce,
-  // firstName: "Cecilia",
-  // lastName: "Ramirez",
-  // customFields: {
-  //   initialcourse: "Introduction to Scratch",
-  //   childage: "8",
-  //   childlastname: "Ramirez",
-  //   childfirstname: "Isabella"  
-  // }
-  // }, function (err, result) {
-  //   if (result.success) {
-  //     var token = result.customer.paymentMethods[0].token;
-  //       gateway.subscription.create({
-  //         paymentMethodToken: token,
-  //         planId: "four_month_membership_id",
-  //         firstBillingDate: startDate
-  //       }, function (err, result) {
-  //         result.success;
-  //       });
-  //     res.redirect('/');
-  //   } else {
-  //     res.redirect('/');
-  //   }
-  // });
-  
-
-  gateway.transaction.sale({
-    amount: amount,
-    paymentMethodNonce: nonce
+  gateway.customer.create({
+  paymentMethodNonce: nonce,
+  firstName: "Federico",
+  lastName: "Ramirez",
+  customFields: {
+    initialcourse: "Introduction to Scratch",
+    childage: "8",
+    childlastname: "Ramirez",
+    childfirstname: "Isabella"  
+  }
   }, function (err, result) {
-    if (result.success || result.transaction) {
-      res.redirect('checkouts/' + result.transaction.id);
+    if (result.success) {
+      var token = result.customer.paymentMethods[0].token;
+        gateway.subscription.create({
+          paymentMethodToken: token,
+          planId: "four_month_membership_id",
+          firstBillingDate: startDate
+        }, function (err, result) {
+          result.success;
+        });
+      res.redirect('/');
     } else {
-      transactionErrors = result.errors.deepErrors();
-      req.flash('error', {msg: formatErrors(transactionErrors)});
-      res.redirect('checkouts/new');
+      res.redirect('/');
     }
   });
+  
+
+  // gateway.transaction.sale({
+  //   amount: amount,
+  //   paymentMethodNonce: nonce
+  // }, function (err, result) {
+  //   if (result.success || result.transaction) {
+  //     res.redirect('checkouts/' + result.transaction.id);
+  //   } else {
+  //     transactionErrors = result.errors.deepErrors();
+  //     req.flash('error', {msg: formatErrors(transactionErrors)});
+  //     res.redirect('checkouts/new');
+  //   }
+  // });
 });
 
 module.exports = router;
