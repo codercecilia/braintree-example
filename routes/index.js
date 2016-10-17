f587a613a15738d8c77fc5c5fe3a5e38224b213a
codercecilia <cecilia@coder-kids.com>
2016-10-17 18:19:56 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -77,6 +77,7 @@ router.post('/checkouts', function (req, res) {
   var childAge = req.body.childage;
   var initialCourse = req.body.initialcourse;
   var nonce = req.body.payment_method_nonce;
+  var startDate = new Date(Date.UTC(2017, 2, 16, 0, 0, 0));
 
   gateway.customer.create({
   paymentMethodNonce: nonce,
@@ -96,7 +97,9 @@ router.post('/checkouts', function (req, res) {
         var token = result.customer.paymentMethods[0].token;
         gateway.subscription.create({
           paymentMethodToken: token,
-          planId: plan
+          planId: plan,
+          firstBillingDate: startDate
+          
         }, function (err, result) {
             if (result.success || result.subscription) {
               res.redirect('checkouts/' + result.subscription.id);
